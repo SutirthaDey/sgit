45a131fcf7f8dd684b3e32f1bf539bb68a2b2d2b
SutirthaDey <deysutirtha31@gmail.com>
2022-06-25 04:42:27 +0530

    added delete and edit functionality using axios

--- registration_page_dynamic/crud-booking_app/axios_crud_bookingApp/main.js
+++ registration_page_dynamic/crud-booking_app/axios_crud_bookingApp/main.js
@@ -1,13 +1,24 @@
 const form = document.getElementById('my-form');
+let updateId;
+let updateLi;
 
 const onDelete = (li,id)=>{
- axios.delete(`https://crudcrud.com/api/f95614ed8854451db9fe1973069120f8/bookings/${id}`)
+ axios.delete(`https://crudcrud.com/api/c208d57f2d1d40c5a63b96e7912096ba/bookings/${id}`)
  .then(()=> li.remove())
  .catch((e)=>console.log(e));
 }
 
-const onEdit = (e)=>{
+const onEdit = (_id,_name,_email,li)=>{
+ const name = document.getElementById('name');
+ const email = document.getElementById('email');
+ const btn = document.getElementById('btn')
 
+ name.value = _name;
+ email.value = _email;
+ btn.value = "Edit";
+
+ updateId = _id;
+ updateLi = li;
 }
 const createPost = ({_id,name,email})=>{
     const userList  = document.getElementById('items');
@@ -15,7 +26,7 @@ const createPost = ({_id,name,email})=>{
     const del = document.createElement('button');
     const edit = document.createElement('button');
     del.addEventListener('click',(e)=>onDelete(li,_id));
-    edit.addEventListener('click',(e)=>onEdit(li,_id));
+    edit.addEventListener('click',(e)=>onEdit(_id,name,email,li));
     const text = document.createTextNode(`${name}`);
     del.appendChild(document.createTextNode(`Delete`));
     edit.appendChild(document.createTextNode(`Edit`));
@@ -36,21 +47,43 @@ const showPosts = (bookings)=>{
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    const btn = document.getElementById('btn');
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
     const obj = {
         name: e.target.name.value,
         email: e.target.email.value
     }
-    
-    axios.post('https://crudcrud.com/api/f95614ed8854451db9fe1973069120f8/bookings', obj)
+
+    if(btn.value === 'Submit')
+    {  
+    axios.post('https://crudcrud.com/api/c208d57f2d1d40c5a63b96e7912096ba/bookings', obj)
     .then((res)=> {
         console.log(res);
-        createPost(res.data)
+        createPost(res.data);
+        name.value = '';
+        email.value= '';
     })
     .catch((e)=> console.log(e));
+   }
+   else
+   {
+    axios.put(`https://crudcrud.com/api/c208d57f2d1d40c5a63b96e7912096ba/bookings/${updateId}`, obj)
+    .then(()=>{
+        updateLi.childNodes[0].nodeValue = obj.name;
+        updateLi.childNodes[1].nodeValue = obj.email;
+        name.value = '';
+        email.value = '';
+        btn.value = "Submit";
+    })
+    .catch(()=>{
+        console.log(e);
+    })
+   }
 })
 
 document.addEventListener('DOMContentLoaded',()=>{
-    axios.get('https://crudcrud.com/api/f95614ed8854451db9fe1973069120f8/bookings')
+    axios.get('https://crudcrud.com/api/c208d57f2d1d40c5a63b96e7912096ba/bookings')
     .then((res)=> showPosts(res.data));
 })
 
