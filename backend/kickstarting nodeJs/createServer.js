fe93deb976101f67b209f3cc517b236f0622cd31
SutirthaDey <deysutirtha31@gmail.com>
2022-06-30 16:00:43 +0530

    nodemon and package.json

--- backend/kickstarting nodeJs/createServer.js
+++ backend/kickstarting nodeJs/createServer.js
@@ -4,6 +4,9 @@ const http = require('http');
 
 const rqListener = (req,res)=>{
     console.log('Sutirtha Dey');
+    res.write(`<html>`);
+    res.write(`<body><h1>Hi</h1></body>`)
+    res.write(`</html>`);
 }
 
 // rqListener is the function managed by event loop get executed wheneven
@@ -15,4 +18,7 @@ const server = http.createServer(rqListener);
 
 
 // Listens for any particular port
-server.listen(4000);
+
+server.listen(4000,()=>{
+    console.log('Listening on port 4000')
+});
